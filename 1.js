b87b62e5fe4ef39fe52901f81d78bd7668a3b31f
Adwaith.k <102245239+adwaith0007@users.noreply.github.com>
2023-07-24 13:00:02 +0530

    3nd day

---
+++ 1.js
@@ -0,0 +1,27 @@
+// var x = {
+//     name : "john"
+// };
+
+// var result =(function(){
+//     delete x.name;
+//     return x.name;
+// })();
+
+// console.log(result);
+
+// var a =10 ;
+// let a=20;
+// if (a===10){
+//     var a=20;
+
+// }
+
+// console.log(a);
+
+function display (){
+    var a= b=10;
+}
+display();
+console.log("a" .a);
+console.log("b",b);
+
