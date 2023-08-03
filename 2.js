801ad2806c81ba2b6c5c3d42b85efe77fe68107d
Adwaith.k <102245239+adwaith0007@users.noreply.github.com>
2023-08-03 12:55:07 +0530

    7nd day

---
+++ 2.js
@@ -0,0 +1,22 @@
+let nums= [2,7,11,15];
+let target=9;
+
+
+
+var twoSum = function(nums, target) {
+    
+
+    for(let i=0; i<nums.length;i++)
+    {
+        for(j=i+1; j<nums.length;j++)
+        {
+            if(nums[i]+nums[j]==target)
+            {
+                console.log(i,j)
+            }
+        }
+    }
+     
+ };
+
+ twoSum(nums, target)
---
+++ 9. Palindrome Number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+
+    let y= x.toString().split('').reverse().join('');
+ 
+    return(x.toString()===y)
+     
+ };
+ 
